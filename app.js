61b3fe799a7a71e68c5496db05a5803fde2c7604
DefinitelyNotAbhishek <110118289+DefinitelyNotAbhishek@users.noreply.github.com>
2024-05-16 23:28:45 +0530

    external react

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello Is this React?"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
